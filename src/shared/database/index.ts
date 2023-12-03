dd030cced44b0cc95c2815443428b034898423b0
duduu202 <Edu.Ap.Oliveira2002@gmail.com>
2023-12-03 00:46:33 -0300

    fix: add cache again

--- src/shared/database/index.ts
+++ src/shared/database/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { PrismaClient } from '@prisma/client';
-// import { withAccelerate } from '@prisma/extension-accelerate';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 export const prisma = new PrismaClient({
   // log: ['query'],
-}); // .$extends(withAccelerate());
+}).$extends(withAccelerate());
